90b6c44d6cc56eee73fed2913b6275983b43b740
Li-mz <mustarfar@163.com>
2020-12-09 22:39:55 +0800

    add stationary SNR

--- detector_response/SNR.py
+++ detector_response/SNR.py
@@ -1,11 +1,10 @@
 # %%
 import numpy as np
 
-import bilby
 from .response import get_detector_response
 
 from .typing import HarmonicMode, ModeArray
-from .typing import Interferometer, TriangularInterferometer, WaveformGenerator
+from .typing import Ifo, Interferometer, TriangularInterferometer, WaveformGenerator
 from typing import Dict, Callable, Iterable, Union, Any
 
 # %%
@@ -17,8 +16,7 @@ def single_ifo_SNR(ifo: Interferometer, mode_array: ModeArray,
     return np.sqrt(ifo.optimal_snr_squared(signal)).real
 
 
-def SNR(ifo: Union[Interferometer, TriangularInterferometer],
-        mode_array: ModeArray,
+def SNR(ifo: Ifo, mode_array: ModeArray,
         generator_from_mode: Callable[[HarmonicMode], WaveformGenerator],
         parameters: Dict, **kwargs):
     parameters = parameters.copy()
@@ -27,3 +25,21 @@ def SNR(ifo: Union[Interferometer, TriangularInterferometer],
         return single_ifo_SNR(ifo, mode_array, generator_from_mode, parameters)
     elif isinstance(ifo, TriangularInterferometer):
         return np.sqrt(sum([single_ifo_SNR(ifo_i, mode_array, generator_from_mode, parameters)**2 for ifo_i in ifo])).real
+
+# %%
+
+def single_ifo_SNR_stationary(ifo: Interferometer, waveform_generator: WaveformGenerator,
+                              parameters: Dict):
+    waveform = waveform_generator.frequency_domain_strain(parameters)
+    signal=ifo.get_detector_response(waveform, parameters)
+    return np.sqrt(ifo.optimal_snr_squared(signal)).real
+
+
+def SNR_stationary(ifo: Ifo, waveform_generator: WaveformGenerator,
+                   parameters: Dict, **kwargs):
+    parameters = parameters.copy()
+    parameters.update(kwargs)
+    if isinstance(ifo, Interferometer):
+        return single_ifo_SNR_stationary(ifo, waveform_generator, parameters)
+    elif isinstance(ifo, TriangularInterferometer):
+        return np.sqrt(sum([single_ifo_SNR_stationary(ifo_i, waveform_generator, parameters)**2 for ifo_i in ifo])).real
--- detector_response/typing.py
+++ detector_response/typing.py
@@ -1,6 +1,8 @@
 from bilby.gw import WaveformGenerator
 from bilby.gw.detector import Interferometer, TriangularInterferometer, InterferometerList
-from typing import Iterable
+from typing import Iterable, Union
 
 HarmonicMode = Iterable[int]
 ModeArray = Iterable[HarmonicMode]
+
+Ifo = Union[Interferometer, TriangularInterferometer]
